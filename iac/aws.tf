2498e4e7ef12daf93d895c24d60f22a3fb678e69
dev <noreply@noreply.local>
2024-10-10 23:32:50 +0100

    test fix

--- iac/aws.tf
+++ iac/aws.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = "my-public-bucket"
-  acl    = "public-read"  // Dangerous, publicly accessible
+  acl    = "private"  // Dangerous, publicly accessible
 
   tags = {
     Name = "My bucket"
