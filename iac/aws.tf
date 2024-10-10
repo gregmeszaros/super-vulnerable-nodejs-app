ccd8110bdc9189d75ba49983db99ecd8893f1cfd
dev <noreply@noreply.local>
2024-10-10 23:35:29 +0100

    add it back

--- iac/aws.tf
+++ iac/aws.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = "my-public-bucket"
-  acl    = "private"  // Dangerous, publicly accessible
+  acl    = "public-read"  // Dangerous, publicly accessible
 
   tags = {
     Name = "My bucket"
